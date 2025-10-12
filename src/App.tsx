77930315e132b42dba65501aefbdc5c0e42002df
Daniel <d-dog@d-dog.se>
2025-10-12 20:14:46 +0200

    arg pirat

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,14 @@ import knarrSample from "./assets/knarr.mp3";
 import vindSample from "./assets/vind.mp3";
 import kanonSample from "./assets/kanon.mp3";
 import temaSample from "./assets/tema-loop.mp3";
+import toapapperSample from "./assets/toapapper.mp3";
 
 const skeppsKlocka = new SamplePlayer(skeppsKlockaSample);
 const knarr = new SamplePlayer(knarrSample);
 const vind = new SamplePlayer(vindSample);
+
 const kanon = new SamplePlayer(kanonSample);
+const toapapper = new SamplePlayer(toapapperSample);
 const temaLoop = new SamplePlayer(temaSample);
 knarr.toggleLooping();
 vind.toggleLooping();
@@ -22,6 +25,10 @@ const oneShots = [
   {
     player: kanon,
     name: "Kanon",
+  },
+  {
+    player: toapapper,
+    name: "Arg pirat",
   },
 ];
 const loops = [
@@ -64,11 +71,13 @@ function App() {
             <h2>Ljud</h2>
             <div className="rope-line"></div>
           </div>
-          {oneShots.map(({ player, name }) => (
-            <button key={name} onClick={() => player.play()}>
-              {name}
-            </button>
-          ))}
+          <div className="buttons">
+            {oneShots.map(({ player, name }) => (
+              <button key={name} onClick={() => player.play()}>
+                {name}
+              </button>
+            ))}
+          </div>
         </div>
 
         <div className="section">
@@ -77,25 +86,27 @@ function App() {
             <h2>Bakgrunder</h2>
             <div className="rope-line"></div>
           </div>
-          {loops.map(({ player, name }, index) => (
-            <button
-              key={name}
-              className={loopsPlaying[index] ? "playing" : ""}
-              onClick={() => {
-                const isPlaying = loopsPlaying[index];
-                if (isPlaying) {
-                  player.stopAll();
-                } else {
-                  player.resumeContextIfNeeded().then(() => player.play());
-                }
-                const newLoopsPlaying = [...loopsPlaying];
-                newLoopsPlaying[index] = !isPlaying;
-                setLoopsPlaying(newLoopsPlaying);
-              }}
-            >
-              {loopsPlaying[index] ? "⏸" : "▶"} {name}
-            </button>
-          ))}
+          <div className="buttons">
+            {loops.map(({ player, name }, index) => (
+              <button
+                key={name}
+                className={loopsPlaying[index] ? "playing" : ""}
+                onClick={() => {
+                  const isPlaying = loopsPlaying[index];
+                  if (isPlaying) {
+                    player.stopAll();
+                  } else {
+                    player.resumeContextIfNeeded().then(() => player.play());
+                  }
+                  const newLoopsPlaying = [...loopsPlaying];
+                  newLoopsPlaying[index] = !isPlaying;
+                  setLoopsPlaying(newLoopsPlaying);
+                }}
+              >
+                {loopsPlaying[index] ? "⏸" : "▶"} {name}
+              </button>
+            ))}
+          </div>
         </div>
       </div>
     </>
